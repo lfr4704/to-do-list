4feb246229e6c7e98164eb4ffd1a4b0845505316
Luis Robles <lfr4704@gmail.com>
2018-04-10 18:09:11 +0000

    revised

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const fs = require('fs');
-const express = require('express');
-let bodyParser = require('body-parser'); //this helps understand object thru jason from the front end
+const express = require('express'); // npm install express
+let bodyParser = require('body-parser'); //this helps understand object thru jason from the front end. this is how to install it: npm install body-parser
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http); //npm install socket.io --save
+let mongoose = require('mongoose'); //npm install -S mongoose
 
 //express routes
 app.use('/client', express.static('client'));
 app.use('/js', express.static('client/js'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
 http.listen(8080);
 
 console.log("server is running");
+
+// this is a listener when someone ask for submitNote
+app.post('/submitNote', function (req, res) {
+    //console.log('something was submitted');
+    console.log(req.body);
+});
